d68f46b812b50756f8bb49720bbb8e8a2f5e8c3b
Liam W <l.s.w5559@gmail.com>
2017-06-10 02:35:48 +0100

    Initial class extension stuff

---
+++ XF/Admin/Controller/UserGroup.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SV\UserMentionsImprovements\XF\Admin\Controller;
+
+class UserGroup extends XFCP_UserGroup
+{
+	protected function userGroupSaveProcess(\XF\Entity\UserGroup $userGroup)
+	{
+		$form = parent::userGroupSaveProcess($userGroup);
+
+		$input = $this->filter([
+			'sv_mentionable' => 'bool',
+			'sv_private' => 'bool',
+			'sv_avatar_s' => 'str',
+			'sv_avatar_l' => 'str'
+		]);
+
+		$form->setup(function () use ($userGroup, $input)
+		{
+			$userGroup->bulkSet($input);
+		});
+
+		return $form;
+	}
+}
---
+++ _output/extension_hint.php
@@ -0,0 +1,14 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace SV\UserMentionsImprovements\XF\Admin\Controller
+{
+	class XFCP_UserGroup extends \XF\Admin\Controller\UserGroup {}
+}
+
+namespace SV\UserMentionsImprovements\XF\Pub\Controller
+{
+	class XFCP_Account extends \XF\Pub\Controller\Account {}
+}
