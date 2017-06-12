e88132a7b65c1deb703e9ef75985a22869e0711c
Liam W <l.s.w5559@gmail.com>
2017-06-12 03:55:02 +0100

    Unrepeat some code. Reports usergroup tagging alert text.

--- Repository/UserMentions.php
+++ Repository/UserMentions.php
@@ -18,4 +18,46 @@ class UserMentions extends Repository
 
 		return $users->fetch();
 	}
+
+	public function mergeUserGroupMembersIntoUsersArray(array $users, $mentionedUserGroups)
+	{
+		if (!$mentionedUserGroups)
+		{
+			return $users;
+		}
+
+		$additionalUsers = \XF::db()->fetchAllKeyed('
+			SELECT DISTINCT user.user_id, user.username, relation.user_group_id
+			FROM xf_user AS user
+			LEFT JOIN xf_user_group_relation AS relation ON relation.user_id = user.user_id
+            WHERE relation.user_group_id IN (' . \XF::db()->quote(array_keys($mentionedUserGroups)) . ')
+		', 'user_id');
+
+		if (!$additionalUsers)
+		{
+			return $users;
+		}
+
+		$mentionedUgUsers = [];
+
+		foreach ($additionalUsers AS $userId => $additionalUser)
+		{
+			if (isset($users[$userId]))
+			{
+				continue;
+			}
+
+			$users[$userId] = [
+				'user_id' => $additionalUser['user_id'],
+				'username' => $additionalUser['username'],
+				'lower' => strtolower($additionalUser['username'])
+			];
+
+			$mentionedUgUsers[$userId] = $mentionedUserGroups[$additionalUser['user_group_id']]['title'];
+		}
+
+		\XF::app()->sv_userGroupMentionedIds = $mentionedUgUsers;
+
+		return $users;
+	}
 }
--- XF/Service/StructuredText/Preparer.php
+++ XF/Service/StructuredText/Preparer.php
@@ -33,46 +33,8 @@ class Preparer extends XFCP_Preparer
 			return $users;
 		}
 
-		$mentionedUsergroupIds = array_keys($this->getMentionedUserGroups());
-
-		if (!$mentionedUsergroupIds)
-		{
-			return $users;
-		}
-
-		$additionalUsers = \XF::db()->fetchAllKeyed('
-			SELECT DISTINCT user.user_id, user.username, relation.user_group_id
-			FROM xf_user AS user
-			LEFT JOIN xf_user_group_relation AS relation ON relation.user_id = user.user_id
-            WHERE relation.user_group_id IN (' . \XF::db()->quote($mentionedUsergroupIds) . ')
-		', 'user_id');
-
-		if (!$additionalUsers)
-		{
-			return $users;
-		}
-
-		$mentionedUgUsers = [];
-
-		$userGroups = $this->getMentionedUserGroups();
-
-		foreach ($additionalUsers AS $userId => $additionalUser)
-		{
-			if (isset($users[$userId]))
-			{
-				continue;
-			}
-
-			$users[$userId] = [
-				'user_id' => $additionalUser['user_id'],
-				'username' => $additionalUser['username'],
-				'lower' => strtolower($additionalUser['username'])
-			];
-
-			$mentionedUgUsers[$userId] = $userGroups[$additionalUser['user_group_id']]['title'];
-		}
-
-		\XF::app()->sv_userGroupMentionedIds = $mentionedUgUsers;
+		$users = \XF::app()->repository('SV\UserMentionsImprovements:UserMentions')
+			->mergeUserGroupMembersIntoUsersArray($users, $this->getMentionedUserGroups());
 
 		return $users;
 	}
