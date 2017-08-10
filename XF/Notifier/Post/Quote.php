83b46d3a2ae252e584ac30eaa777159696b132a2
Liam W <l.s.w5559@gmail.com>
2017-08-10 16:06:24 +0100

    Fixes related to changes with notifications (alerts and emails)

---
+++ XF/Notifier/Post/Quote.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SV\UserMentionsImprovements\XF\Notifier\Post;
+
+class Quote extends XFCP_Quote
+{
+	public function sendEmail(\XF\Entity\User $user)
+	{
+		$params = [
+			'post' => $this->post,
+			'thread' => $this->post->Thread,
+			'forum' => $this->post->Thread->Forum,
+			'receiver' => $user
+		];
+
+		$this->app->mailer()->newMail()
+			->setToUser($user)
+			->setTemplate('sv_usermentionsimprovements_quote', $params)
+			->queue();
+	}
+}
