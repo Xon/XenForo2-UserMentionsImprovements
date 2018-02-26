2532798090a7e71002f49b661a0f9036beda2114
Xon <tazipper1@hotmail.com>
2018-02-26 12:33:05 +0800

    Only rebuild changes if required when editing a usergroup

--- upload/src/addons/SV/UserMentionsImprovements/XF/Service/UpdatePermissions.php
+++ upload/src/addons/SV/UserMentionsImprovements/XF/Service/UpdatePermissions.php
@@ -9,8 +9,76 @@ namespace SV\UserMentionsImprovements\XF\Service;
  */
 class UpdatePermissions extends XFCP_UpdatePermissions
 {
+    protected $hadChanges = false;
+
+    /**
+     * @param \XF\Entity\Permission                                                                   $permission
+     * @param mixed                                                                                   $value
+     * @param \XF\Mvc\Entity\Entity|\XF\Entity\PermissionEntry|\XF\Entity\PermissionEntryContent|null $entry
+     * @return null|\XF\Entity\PermissionEntry|\XF\Entity\PermissionEntry|\XF\Entity\PermissionEntryContent|\XF\Mvc\Entity\Entity
+     * @throws \XF\PrintableException
+     */
+    protected function writeEntry(\XF\Entity\Permission $permission, $value, \XF\Mvc\Entity\Entity $entry = null)
+    {
+        if ($value == 'unset' || $value === '0' || $value === 0)
+        {
+            if ($entry)
+            {
+                $entry->delete();
+            }
+
+            return null;
+        }
+
+        if (!$entry)
+        {
+            if ($this->contentType)
+            {
+                /** @var \XF\Entity\PermissionEntryContent $entry */
+                $entry = $this->em()->create('XF:PermissionEntryContent');
+                $entry->content_type = $this->contentType;
+                $entry->content_id = $this->contentId;
+            }
+            else
+            {
+                /** @var \XF\Entity\PermissionEntry $entry */
+                $entry = $this->em()->create('XF:PermissionEntry');
+            }
+
+            $entry->permission_group_id = $permission->permission_group_id;
+            $entry->permission_id = $permission->permission_id;
+        }
+
+        $entry->user_id = $this->user ? $this->user->user_id : 0;
+        $entry->user_group_id = $this->userGroup ? $this->userGroup->user_group_id : 0;
+
+        if ($permission->permission_type == 'integer')
+        {
+            $entry->permission_value = 'use_int';
+            $entry->permission_value_int = intval($value);
+        }
+        else
+        {
+            $entry->permission_value = $value;
+            $entry->permission_value_int = 0;
+        }
+
+        $entry->saveIfChanged($saved);
+        if ($saved)
+        {
+            $this->hadChanges = true;
+        }
+
+        return $entry;
+    }
+
     public function triggerCacheRebuild()
     {
+        if (!$this->hadChanges)
+        {
+            return;
+        }
+
         /** @var \XF\Repository\PermissionCombination $combinationRepo */
         $combinationRepo = $this->repository('XF:PermissionCombination');
 
