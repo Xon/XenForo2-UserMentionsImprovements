6103181c5bc62922a4b799937bf879779ec9e2a9
Xon <tazipper1@hotmail.com>
2018-04-10 14:37:21 +0800

    Tweak js

--- upload/src/addons/SV/UserMentionsImprovements/_files/js/sv/mentionimprovements/editor-integration.js
+++ upload/src/addons/SV/UserMentionsImprovements/_files/js/sv/mentionimprovements/editor-integration.js
@@ -5,8 +5,7 @@
  * file that was distributed with this source code.
  */
 
-/** @param {jQuery} $ jQuery Object */
-!function($, window, document, _undefined)
+(function($, window, document, _undefined)
 {
     XF.adjustHtmlForRteOld = XF.adjustHtmlForRte;
     XF.adjustHtmlForRte = function(content)
@@ -15,10 +14,10 @@
 
         content = content.replace(/([\w\W]|^)<a\s[^>]*data-usergroup-id="\d+"\s+data-groupname="([^"]+)"[^>]*>([\w\W]+?)<\/a>/gi,
             function(match, prefix, user, username) {
-                return prefix + (prefix == '@' ? '' : '@') + username.replace(/^@/, '');
+                return prefix + (prefix === '@' ? '' : '@') + username.replace(/^@/, '');
             }
         );
 
         return content;
     };
-}(jQuery, window, document);
+}(jQuery, window, document));
