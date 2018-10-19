6dac3f7cce5ad2b3529dd3dd60e3913e9008ea4d
Xon <tazipper1@hotmail.com>
2018-10-19 10:15:05 +0800

    GUI option to kick off a permission rebuild

---
+++ upload/src/addons/SV/UserMentionsImprovements/XF/Admin/Controller/Tools.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace SV\UserMentionsImprovements\XF\Admin\Controller;
+
+
+
+use XF\Mvc\Reply\Redirect;
+
+/**
+ * Extends \XF\Admin\Controller\Tools
+ */
+class Tools extends XFCP_Tools
+{
+    public function actionCleanUpPermissions()
+    {
+        $response = parent::actionCleanUpPermissions();
+        if ($response instanceof Redirect)
+        {
+            if ($this->filter('sv_rebuild_perms', 'bool'))
+            {
+                $this->app->jobManager()->enqueueUnique('permissionRebuild', 'XF:PermissionRebuild');
+            }
+        }
+
+        return $response;
+    }
+}
--- upload/src/addons/SV/UserMentionsImprovements/_output/extension_hint.php
+++ upload/src/addons/SV/UserMentionsImprovements/_output/extension_hint.php
@@ -5,6 +5,7 @@
 
 namespace SV\UserMentionsImprovements\XF\Admin\Controller
 {
+	class XFCP_Tools extends \XF\Admin\Controller\Tools {}
 	class XFCP_User extends \XF\Admin\Controller\User {}
 	class XFCP_UserGroup extends \XF\Admin\Controller\UserGroup {}
 }
