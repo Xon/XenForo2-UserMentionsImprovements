62d871029fcd3dca5e2399d122090e31a0d01638
Xon <tazipper1@hotmail.com>
2019-03-16 09:07:00 +0800

    XF2.1.1 compatibility bugfix

--- upload/src/addons/SV/UserMentionsImprovements/XF/BbCode/ProcessorAction/MentionUsers.php
+++ upload/src/addons/SV/UserMentionsImprovements/XF/BbCode/ProcessorAction/MentionUsers.php
@@ -2,14 +2,52 @@
 
 namespace SV\UserMentionsImprovements\XF\BbCode\ProcessorAction;
 
+use XF\BbCode\Parser;
+use XF\BbCode\RuleSet;
+
 class MentionUsers extends XFCP_MentionUsers
 {
     protected $mentionedUserGroups = [];
 
+    /**
+     * XF2.0- XF2.1
+     *
+     * @param $string
+     * @return string|null
+     */
     public function filterFinal($string)
     {
+        /** @noinspection PhpUndefinedMethodInspection */
         $string = parent::filterFinal($string);
+        $string = $this->extractMentionedUserGroups($string);
 
+        return $string;
+    }
+
+    /**
+     * XF2.1.1+
+     *
+     * @param string  $string
+     * @param Parser  $parser
+     * @param RuleSet $rules
+     * @param array   $options
+     * @return string|null
+     */
+    public function filterInput($string, Parser $parser, RuleSet $rules, array &$options)
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        $string = parent::filterInput($string, $parser, $rules, $options);
+        $string = $this->extractMentionedUserGroups($string);
+
+        return $string;
+    }
+
+    /**
+     * @param $string
+     * @return string|null
+     */
+    protected function extractMentionedUserGroups($string)
+    {
         /** @var \SV\UserMentionsImprovements\XF\Str\Formatter $formatter */
         $formatter = $this->formatter;
         if (!\is_callable([$formatter, 'getUserGroupMentionFormatter']))
