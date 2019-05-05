17f988412045688461c4b638bc4eb0ae083b5045
Xon <tazipper1@hotmail.com>
2019-05-06 00:09:44 +0800

    Remove changes to rebuildCombinationContent

--- upload/src/addons/SV/UserMentionsImprovements/_output/extension_hint.php
+++ upload/src/addons/SV/UserMentionsImprovements/_output/extension_hint.php
@@ -43,11 +43,6 @@ namespace SV\UserMentionsImprovements\XF\Notifier\Post
 	class XFCP_Quote extends \XF\Notifier\Post\Quote {}
 }
 
-namespace SV\UserMentionsImprovements\XF\Permission
-{
-	class XFCP_Builder extends \XF\Permission\Builder {}
-}
-
 namespace SV\UserMentionsImprovements\XF\Pub\Controller
 {
 	class XFCP_Account extends \XF\Pub\Controller\Account {}
