c4e3fb904eff7a0693ecf6c3664399ecdd663375
Xon <tazipper1@hotmail.com>
2020-07-24 09:45:49 +0800

    Fix usage of class_alias

--- upload/src/addons/SV/UserMentionsImprovements/XF/Repository/UserAlert.php
+++ upload/src/addons/SV/UserMentionsImprovements/XF/Repository/UserAlert.php
@@ -4,9 +4,9 @@ namespace SV\UserMentionsImprovements\XF\Repository;
 
 if (\XF::$versionId < 2020000)
 {
-    \class_alias('SV\UserMentionsImprovements\XF\Repository\XF2\UserAlert', 'SV\UserMentionsImprovements\XF\Repository\UserAlert', false);
+    \class_alias('SV\UserMentionsImprovements\XF\Repository\XF2\UserAlert', 'SV\UserMentionsImprovements\XF\Repository\UserAlert');
 }
 else
 {
-    \class_alias('SV\UserMentionsImprovements\XF\Repository\XF22\UserAlert', 'SV\UserMentionsImprovements\XF\Repository\UserAlert', false);
+    \class_alias('SV\UserMentionsImprovements\XF\Repository\XF22\UserAlert', 'SV\UserMentionsImprovements\XF\Repository\UserAlert');
 }
