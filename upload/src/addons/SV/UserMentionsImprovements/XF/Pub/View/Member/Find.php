cc203f205312a46177c7fc4f1d2165c494e3e3c3
Xon <tazipper1@hotmail.com>
2020-08-28 22:38:42 +0800

    Add-on compatibility improvement

--- upload/src/addons/SV/UserMentionsImprovements/XF/Pub/View/Member/Find.php
+++ upload/src/addons/SV/UserMentionsImprovements/XF/Pub/View/Member/Find.php
@@ -8,18 +8,21 @@ class Find extends XFCP_Find
     {
         $response = parent::renderJson();
 
-        foreach ($this->params['userGroups'] AS $usergroup)
+        if (isset($this->params['userGroups']))
         {
-            /** @var \SV\UserMentionsImprovements\XF\Entity\UserGroup $usergroup */
-            array_unshift(
-                $response['results'],
-                [
-                    'id'       => $usergroup->user_group_id,
-                    'iconHtml' => $usergroup->icon_html,
-                    'text'     => $usergroup->title,
-                    'q'        => $this->params['q'],
-                ]
-            );
+            foreach ($this->params['userGroups'] as $usergroup)
+            {
+                /** @var \SV\UserMentionsImprovements\XF\Entity\UserGroup $usergroup */
+                array_unshift(
+                    $response['results'],
+                    [
+                        'id'       => $usergroup->user_group_id,
+                        'iconHtml' => $usergroup->icon_html,
+                        'text'     => $usergroup->title,
+                        'q'        => $this->params['q'],
+                    ]
+                );
+            }
         }
 
         return $response;
