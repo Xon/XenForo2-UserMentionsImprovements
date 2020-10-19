16e373f2bfa3060f2cf16e5ebb7d698b017137ff
Xon <tazipper1@hotmail.com>
2020-10-20 06:46:01 +0800

    Compatibility update when resolving an extended class to the root class

--- upload/src/addons/SV/UserMentionsImprovements/XF/Repository/UserAlert.php
+++ upload/src/addons/SV/UserMentionsImprovements/XF/Repository/UserAlert.php
@@ -2,11 +2,9 @@
 
 namespace SV\UserMentionsImprovements\XF\Repository;
 
-if (\XF::$versionId < 2020000)
-{
-    \class_alias('SV\UserMentionsImprovements\XF\Repository\XF2\UserAlert', 'SV\UserMentionsImprovements\XF\Repository\UserAlert');
-}
-else
-{
-    \class_alias('SV\UserMentionsImprovements\XF\Repository\XF22\UserAlert', 'SV\UserMentionsImprovements\XF\Repository\UserAlert');
-}
+\SV\StandardLib\Helper::repo()->aliasClass(
+    'SV\UserMentionsImprovements\XF\Repository\UserAlert',
+    \XF::$versionId < 2020000
+        ? 'SV\UserMentionsImprovements\XF\Repository\XF2\UserAlert'
+        : 'SV\UserMentionsImprovements\XF\Repository\XF22\UserAlert'
+);
