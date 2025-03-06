2ece41704927909f9d0601c5f9364a7be6bf8fb4
Xon <635541+Xon@users.noreply.github.com>
2025-03-07 07:15:38 +0800

    XF2.3 export

---
+++ upload/src/addons/SV/UserMentionsImprovements/_output/option_hint.php
@@ -0,0 +1,26 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property bool|null $svBlockMentionRenderingOnNoPermissions Block mention rendering if the user has no permissions to use mentions
+ * @property bool|null $svUMIPermDeniedOnViewGroup Return permission denied instead of not found if lacking permissions to view a user group
+ * @property positive-int|null $svUMI_usersPerPage Public user group - users per page
+ * @property string|null $sv_default_group_avatar_l Default usergroup avatar (large)
+ * @property string|null $sv_default_group_avatar_s Default usergroup avatar (small)
+ * @property bool|null $sv_limit_quote_emails Send quote emails only on unreplied threads
+ * @property int|null $sv_mention_snippet_length Mention/Quote Snippet length
+ * @property bool|null $sv_send_email_on_quote Allow quoted alert emails
+ * @property bool|null $sv_send_email_on_tagging Allow mention alert emails
+ */
+class Options
+{
+}
