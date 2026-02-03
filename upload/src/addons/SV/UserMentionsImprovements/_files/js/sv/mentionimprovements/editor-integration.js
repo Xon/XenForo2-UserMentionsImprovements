d84af822fab9b97c63f4ac3f8c35c1077ecc3704
Xon <635541+Xon@users.noreply.github.com>
2026-02-03 11:11:41 +0800

    Fix usergroup mention's editor integration causing a non-deterministic javascript stack overflow when using the  FireFox plugin Decentraleyes

--- upload/src/addons/SV/UserMentionsImprovements/_files/js/sv/mentionimprovements/editor-integration.js
+++ upload/src/addons/SV/UserMentionsImprovements/_files/js/sv/mentionimprovements/editor-integration.js
@@ -1,9 +1,9 @@
 (function(window, document, _undefined)
 {
-    XF.adjustHtmlForRteOld = XF.adjustHtmlForRte;
+    var adjustHtmlForRteOld = XF.adjustHtmlForRte;
     XF.adjustHtmlForRte = function(content)
     {
-        content = XF.adjustHtmlForRteOld(content);
+        content = adjustHtmlForRteOld(content);
 
         content = content.replace(/([\w\W]|^)<a\s[^>]*data-usergroup-id="\d+"\s+data-groupname="([^"]+)"[^>]*>([\w\W]+?)<\/a>/gi,
             function(match, prefix, user, username) {
